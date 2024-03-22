4fbfc19e2b9a7817781edb7a76a5dd4d5f44ada4
sanket rahangdale <sanketrahangdale1118@gmail.com>
2024-03-22 20:38:45 +0530

    using get method getting list of all the files that exist in a folder

--- routes/s3bucket.router.js
+++ routes/s3bucket.router.js
@@ -113,6 +113,28 @@ router.post("/uploadFileInBucket", Auth.userAuthMiddleware, upload().single("myF
     }
 });
 
+// get list of all files from a Particular bucket
+router.get("/getAllFilesFromParticularBucket", Auth.userAuthMiddleware, async (req, res) => {
+    try {
+        const bucketName = req.body.bucketName;
+        const directoryPath = path.join(`bucketFolder/${bucketName}`);
+        fs.readdir(directoryPath, (err, files) => {
+            if (err) {
+                return res.json({ status: false, message: `${bucketName}, No Such Bucket Found` });
+            }
+            const allfiles = files.filter(file => {
+                const filePath = path.join(directoryPath, file);
+                return fs.statSync(filePath).isFile();
+            });
+            console.log(allfiles);
+            return res.json({ status: true, filesList: allfiles });
+        });
+    } catch (error) {
+        console.log("error------->>", error);
+    }
+});
+
+
 
 
 
