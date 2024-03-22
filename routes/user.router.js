c7f0c1b6b2c8f7273dfdb105c57e64fe88b0db32
sanket rahangdale <sanketrahangdale1118@gmail.com>
2024-03-22 16:27:28 +0530

    created api and stored api in mongoDb Database

--- routes/user.router.js
+++ routes/user.router.js
@@ -4,16 +4,17 @@ const crypto = require("crypto-js");
 
 router.post('/register' , async (req,res) => {
     try{
-        const {name} = req.body;
-        var str = name + new Date.now();
+        const { name } = req.body;
+        var str = name + Date.now(); // Corrected line
         const apiKey = crypto.SHA256(str).toString().slice(0 , 20);
 
-        const userCreated = new userModel({name , apiKey});
+        const userCreated = new userModel({ name, apiKey });
         await userCreated.save();
 
-        res.json({status: 200 , success: "user Created"});
+        res.json({ status: 200, success: "User created" });
     } catch(error){
-        console.log(error)
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" }); // Send an error response to the client
     }
 });
 
